b3168ee902234a01951adf60e2001841a8722875
Sajjadlaghari <sajjadlaghari723@gmail.com>
2022-07-27 17:52:53 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <a class="navbar-brand" href="#">Navbar</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
+
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav mr-auto">
+         <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home</router-link>
+         </li>
+         <li class="nav-item">
+          <router-link class="nav-link" to="gallery">Gallery</router-link>
+         </li>
+         <li class="nav-item">
+          <router-link class="nav-link" to="about">About</router-link>
+         </li>
+       </ul>
+          <router-link class="nav-link" to="registration" style="color:black; font-weight:bold">Registration</router-link>
+          <router-link class="nav-link" to="login" style="color:black; font-weight:bold">Login</router-link>
+          <a style="color:black; font-weight:bold; cursor:pointer">Logout</a>
+
+      
+     </div>
+   </nav>
+
+
+
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
@@ -19,8 +44,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: process.env.NODE_ENV === 'production' ?
+        'C:/xampp/htdocs/my-app/' :
+        '/'
+        ,
+  transpileDependencies: true,
+  lintOnSave:false,
 })
